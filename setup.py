404e6efeda796d0d81c5a8a2983a86dc18c0dde7
Saman Siadati <33845480+samansiadati@users.noreply.github.com>
2025-07-09 13:56:07 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,29 +4,29 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='saz',
-    version='0.0.9',
-    packages=['saz'],
-    license='MIT',
-    description='Testing installation of Package',
+    name="saz",
+    version="0.0.9",
+    packages=["saz"],
+    license="MIT",
+    description="Testing installation of Package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    author='Saman Siadati',
-    author_email='siadatisaman@gmail.com',
-    url='https://github.com/samansiadati/saz',
+    author="Saman Siadati",
+    author_email="siadatisaman@gmail.com",
+    url="https://github.com/samansiadati/saz",
     project_urls={
         "Bug Tracker": "https://github.com/samansiadati/saz/issues",
     },
     install_requires=[
-        'pandas',
+        "pandas",
     ],
     keywords=["pandas", "manipulation", "saz"],
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Documentation',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Documentation",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
     ],
     download_url="https://github.com/samansiadati/saz/archive/refs/tags/saz.tar.gz",
 )
