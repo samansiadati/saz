94c3d56ee7fa1958220a68539c75c9cb2b7339c0
Saman Siadati <33845480+samansiadati@users.noreply.github.com>
2025-07-09 13:55:04 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,25 @@
-    install_requires=['pandas'],
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='saz',
+    version='0.0.9',
+    packages=['saz'],
+    license='MIT',
+    description='Testing installation of Package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Saman Siadati',
+    author_email='siadatisaman@gmail.com',
+    url='https://github.com/samansiadati/saz',
+    project_urls={
+        "Bug Tracker": "https://github.com/samansiadati/saz/issues",
+    },
+    install_requires=[
+        'pandas',
+    ],
     keywords=["pandas", "manipulation", "saz"],
     classifiers=[
         'Development Status :: 3 - Alpha',
