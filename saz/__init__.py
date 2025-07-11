2066d84241b879ff02efcb57fc9048366aa5426a
Saman Siadati <33845480+samansiadati@users.noreply.github.com>
2025-07-11 23:29:49 +1000

    Update __init__.py

--- saz/__init__.py
+++ saz/__init__.py
@@ -1,2 +1,2 @@
-from .sazi import updated_nm, make_report, make_csv, tbl_schema, tbl_cardin, remove_quotes
+from .sazi import (updated_nm, make_report, make_csv, tbl_schema, tbl_cardin, remove_quotes)
 
